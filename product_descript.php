e62fc1c7672735f13fb841231a98901e08bd77d3
Henry Mitchell Rice <hmrice@silk24.uvm.edu>
2020-11-27 12:11:19 -0500

    added some SQL to fil products page with products from DB, also added SQl to populate product_descript.php with the product they chose

--- product_descript.php
+++ product_descript.php
@@ -3,6 +3,24 @@ include 'top.php';
 ?>
 
 <main>
-    <p>Specific product description, link to purchase</p>  
+    <p>Specific product description, link to add to cart</p>
+
+    <?php
+        $query = "SELECT `pmkProductID`, `fldName`,`fldPrice`,`fldRemainingStock` FROM `tblInventory` WHERE `pmkProductID` = '$_GET[productID]'";
+        //print '<h1>' . $query . '</h1>';
+        if ($thisDatabaseReader->querySecurityOk($query,1,0,2,0,0))
+            {
+                $query = $thisDatabaseReader->sanitizeQuery($query,1,0,1,0,0);
+
+                $records = $thisDatabaseReader->select($query, '');
+
+            }
+        foreach($records as $record)
+        {
+            print '<h1>' . $record['fldName'] . '</h1>';
+            print '<h2>$' . $record['fldPrice'] . '</h2>';
+        }
+
+    ?>
 </main>
 <?php include 'footer.php'; ?>
