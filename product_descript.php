f8a42f41ac4ba48ac6caa3bb61ff09935f46fca7
Henry Mitchell Rice <hmrice@silk24.uvm.edu>
2020-11-27 13:31:31 -0500

    added images to sample data, added some CSS to nav and header

--- product_descript.php
+++ product_descript.php
@@ -6,7 +6,7 @@ include 'top.php';
     <p>Specific product description, link to add to cart</p>
 
     <?php
-        $query = "SELECT `pmkProductID`, `fldName`,`fldPrice`,`fldRemainingStock` FROM `tblInventory` WHERE `pmkProductID` = '$_GET[productID]'";
+        $query = "SELECT `pmkProductID`, `fldName`,`fldPrice`,`fldRemainingStock`, `fldImage` FROM `tblInventory` WHERE `pmkProductID` = '$_GET[productID]'";
         //print '<h1>' . $query . '</h1>';
         if ($thisDatabaseReader->querySecurityOk($query,1,0,2,0,0))
             {
@@ -19,8 +19,15 @@ include 'top.php';
         {
             print '<h1>' . $record['fldName'] . '</h1>';
             print '<h2>$' . $record['fldPrice'] . '</h2>';
+            print '<img src="'. $record['fldImage'] .  '" alt="ProductImage" style="width:128px;height:128px;">';
+            print '<p> We currently have ' . $record['fldRemainingStock'] . ' left in stock </p>';
         }
-
     ?>
+
+    <form method = "get" action = "cart.php">
+        <label>Quantity</label>
+        <input type = 'text' name = '#' size = '2' >
+        <input type = 'submit' value = 'Add to Cart'>
+    </form>
 </main>
 <?php include 'footer.php'; ?>
