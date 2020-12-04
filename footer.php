7451f89c3a978eb242f3cdf2245459245e3b1f55
Aaron Slocum <ajslocum@uvm.edu>
2020-12-03 23:00:36 -0500

    fixed footer

--- footer.php
+++ footer.php
@@ -1,2 +1,4 @@
-<p> <a href="../index.php"> Index.php </a> </p>
-<p>Project by Aaron Slocum, Henry Rice, Emmett O'Connell</p>
+<footer>
+    <p> <a href="../index.php"> Index.php </a> </p>
+    <p>Project by Aaron Slocum, Henry Rice, Emmett O'Connell</p>
+</footer>
