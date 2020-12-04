5e55f9d1e74e710b6902b20866e5edf40302382b
Aaron Slocum <ajslocum@uvm.edu>
2020-12-03 22:57:21 -0500

    fixed some small css

--- header.php
+++ header.php
@@ -1,12 +1,4 @@
 <!-- ###################   Page header #################### -->
-<head>
-<style>
-    .headerBanner {
-      background-color: #734d26;
-      overflow: hidden;
-    }
-</style>
-</head>
 <header class = "headerBanner">
     <h1>Shmead Meads</h1>
     <h3>Final Project - Aaron Slocum, Henry Rice, Emmett O'Connell</h3>
