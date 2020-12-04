7e5eb253d1be81cc2d80272d8d525e1af547aa5c
Henry Rice <hmrice@uvm.edu>
2020-12-04 11:37:39 -0500

    added customer reviews to be displayed on home page

--- index.php
+++ index.php
@@ -12,5 +12,29 @@ include 'top.php';
         <p>"Wow, thank you so much to the guys at Shmead Meads, without them I could never have become this handsome and talented. I owe it all to the hard work these fine brewers are putting in." - Danny DeVito</p>
         <p>"It was ok" - Aaron Slocum's mother</p>
         <p>"My god, this is the betht mead I've ever had" - 10 time heavyweight champion, Mike Tyson </p>
+
+    <?php
+    $query = "SELECT tblReview.pfkCustomerEmail, tblReview.fldStars, tblReview.fldReview, tblCustomers.pmkCustomerEmail, tblCustomers.fldFirstName, tblCustomers.fldLastName 
+                            FROM `tblReview` 
+                            INNER JOIN `tblCustomers` ON tblReview.pfkCustomerEmail = tblCustomers.pmkCustomerEmail
+                            WHERE tblReview.pfkCustomerEmail = tblCustomers.pmkCustomerEmail";
+
+    //print '<p>' . $query . '</p>';
+
+    //$thisDatabaseReader->testSecurityQuery($query);
+    //print '<h1>' . $query . '</h1>';
+    if ($thisDatabaseReader->querySecurityOk($query,1,0,0,0,0))
+    {
+        $query = $thisDatabaseReader->sanitizeQuery($query,0,0,0,0,0);
+
+        $records = $thisDatabaseReader->select($query, '');
+
+    }
+
+    foreach ($records as $record) {
+        print '<p>"'. $record['fldReview'] .'"   - '.$record['fldFirstName'] . ' ' .$record['fldLastName'].'</p>';
+    }
+
+    ?>
 </main>
 <?php include 'footer.php'; ?>
